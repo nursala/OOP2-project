4c268db907c4e42db15b8d766579f0a0c1eacfbd
Amer Abu sair <amerab@std.hac.ac.il>
2025-06-18 22:03:42 +0300

    שששש

--- include/GameObject/Bullet.h
+++ include/GameObject/Bullet.h
@@ -1,22 +1,22 @@
-#pragma once
-
-#include "GameObject/Entity.h"
-
-class World; // Forward declaration to avoid circular dependency
-class Bullet : public Entity {
-public:
-    Bullet(World& world, const sf::Vector2f& position, const sf::Vector2f& direction, float speed);
-
-    void update(float deltaTime) override;
-    //void handleCollision(Entity& other) override;
-    bool shouldDestroy() const;
-
-private:
-    sf::Vector2f m_direction;
-    float m_speed;
-    float m_lifetime = 0.f;
-    const float MAX_LIFETIME = 3.f;
-    bool m_destroy = false;
-
-	sf::CircleShape m_shape; 
-};
+//#pragma once
+//
+//#include "GameObject/Entity.h"
+//
+//class World; // Forward declaration to avoid circular dependency
+//class Bullet : public Entity {
+//public:
+//    Bullet(World& world, const sf::Vector2f& position, const sf::Vector2f& direction, float speed);
+//
+//    void update(float deltaTime) override;
+//    //void handleCollision(Entity& other) override;
+//    bool shouldDestroy() const;
+//
+//private:
+//    sf::Vector2f m_direction;
+//    float m_speed;
+//    float m_lifetime = 0.f;
+//    const float MAX_LIFETIME = 3.f;
+//    bool m_destroy = false;
+//
+//	sf::CircleShape m_shape; 
+//};
